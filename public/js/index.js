9aaf3a843a3f565985de27762931cc85a09f7b64
yanyige <232218719@qq.com>
2016-05-18 01:35:53 +0800

    2016.5.18 首页完成部分，添加imqq中的效果，使用position-attachment: fixed

---
+++ public/js/index.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+
+
+	jQuery.start = function(){
+		var motto = $(".motto");
+		var welcome = $(".welcome");
+		var welcomeRight = $(".welcome-right");
+		var intro = $(".intro");
+
+		intro.hide();
+		welcomeRight.hide();
+
+		motto.animate({left: '15%'}, 'slow');
+		welcome.animate({left: '15%'}, 'slow', function(){
+			welcomeRight.show();
+			welcomeRight.animate({right: '15%'}, 'slow', function(){
+				intro.fadeIn('slow');
+			});
+		});
+	}
+	$.start();
+
+	jQuery.imqq = function(){
+		with(arguments){
+			var ele = arguments;
+			$(window).scroll(function(){
+				var temp = (ele[0].offset().top - $(window).scrollTop()) * 0.03;
+		        var pos = "50% "+temp+"px";
+		        $(".section1Img").css({"background-position":pos});
+			});
+		}
+	}
+	$.imqq($(".section1"));
+
+});
